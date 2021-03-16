5224114b9f2549bd92d2ba37e1bd6e31eb00ed41
Tannnkely <tannnkely@gmail.com>
2021-03-16 22:29:36 +0300

    module5-task2

--- js/main.js
+++ js/main.js
@@ -2,3 +2,4 @@ import './util.js';
 import './data.js';
 import './const.js';
 import './mini-pictures';
+import './big-picture.js';
--- js/pictures.js
+++ js/pictures.js
@@ -1,19 +1,21 @@
 import './data.js';
 import generateObjectsArray from './data.js';
+import openBigPicture from './big-picture.js';
 
 const usersPictures = document.querySelector('.pictures');
 const pictureTemplateFragmet = document.querySelector('#picture').content.querySelector('.picture');
-const usersComments = pictureTemplateFragmet.querySelector('.picture__comments');
-const usersLikes = pictureTemplateFragmet.querySelector('.picture__likes');
+const createUsersImages = function ({url, comments, likes, description}) {
+  const usersImage = pictureTemplateFragmet.cloneNode(true);
+  usersImage.querySelector('.picture__img').src = url;
+  usersImage.querySelector('.picture__comments').textContent = comments.length;
+  usersImage.querySelector('.picture__likes').textContent = likes;
+  usersImage.addEventListener('click', () => {
 
-const createUsersImages = function ({url, comments, likes}) {
-  const usersImages = pictureTemplateFragmet.cloneNode(true);
-  usersImages.querySelector('.picture__img').src = url;
-  usersComments.textContent = comments.length;
-  usersLikes.textContent = likes;
-
-  return usersImages;
+    openBigPicture({url, comments, likes, description});
+  });
+  return usersImage;
 };
+
 
 const renderUsersImages = function () {
   const fragment = document.createDocumentFragment();
